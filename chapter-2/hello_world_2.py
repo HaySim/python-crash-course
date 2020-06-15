a9f927907c4ed71d53d651d2881144dc037dc46a
HaySim <hayley_simmons@outlook.com>
2020-06-15 23:08:00 +0100

    add hello_world_2 and name.py
    add hello_world_2.py and name.py

---
+++ chapter-2/hello_world_2.py
@@ -0,0 +1,17 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+msg = "Hello Python Crash Course reader!"
+print(msg)
+
+message = "Look at that!"
+print(message)
+
+msg = "Look at that!"
+print(msg)
+
+
+
