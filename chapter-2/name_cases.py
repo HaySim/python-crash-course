673c92b1b69f7acb444c11c033e1d9f404489208
HaySim <hayley_simmons@outlook.com>
2020-06-18 00:34:42 +0100

    remove unnecessary whitespaces

--- chapter-2/name_cases.py
+++ chapter-2/name_cases.py
@@ -27,3 +27,29 @@ print(message)
 message = "Michelle Obama once said, “When they go low, we go high.”"
 print(message)
 
+famous_person = "When they go low, we go high."
+print(famous_person)
+
+#\n is a newline:
+name = "Michelle Obama"
+print("Michelle \nObama")
+
+#\t is a tab:
+name = "Michelle Obama"
+print("Michelle \tObama")
+
+#\r Carriage return - takes the cursor to the beginning of the line. Will print as Obamalle:
+name = "Michelle Obama"
+print("Michelle \rObama")
+
+#The Python lstrip() method removes the leading spaces from the left side of the string:
+name = "Michelle Obama"
+print(name.lstrip('Michelle '))
+
+#The Python rstrip() method removes the leading spaces from the right side of the string:
+name = "Michelle Obamalll"
+print(name.rstrip('l'))
+
+#The strip() method removes characters from both left and right:
+name = "Michelle Obama"
+print(name.strip('Obama'))
