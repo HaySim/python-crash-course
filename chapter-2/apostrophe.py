32dd5414377020b842269b4d4482640aa311ccee
HaySim <hayley_simmons@outlook.com>
2020-06-17 01:33:49 +0100

    Syntax error example with apostrophe

---
+++ chapter-2/apostrophe.py
@@ -0,0 +1,9 @@
+# Correct! "
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# Incorrect! ' - Syntax Error
+
+message = 'One of Python's strengths is its diverse community.'
+print(message)
