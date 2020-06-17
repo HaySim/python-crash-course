9099ed00d6ad30d64eac61f2ce8c12faf72afebd
HaySim <hayley_simmons@outlook.com>
2020-06-17 01:14:35 +0100

    implement text formating

---
+++ chapter-2/name_cases.py
@@ -0,0 +1,29 @@
+message = "Hello Eric, would you like to learn some Python today?"
+print(message)
+
+name = "hayley simmons"
+print(name.title())
+
+name = "hayley simmons"
+print(name.upper())
+print(name.lower())
+
+first_name = "hayley"
+last_name = "simmons"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "hayley"
+last_name = "simmons"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+first_name = "hayley"
+last_name = "simmons"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+message = "Michelle Obama once said, “When they go low, we go high.”"
+print(message)
+
