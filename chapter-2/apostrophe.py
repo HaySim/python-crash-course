05a97c1300c66c9d29fb838c57c74639f09d4ec4
HaySim <hayley_simmons@outlook.com>
2020-06-18 00:30:07 +0100

    removed unnecessary whitespaces

--- chapter-2/apostrophe.py
+++ chapter-2/apostrophe.py
@@ -1,9 +1,7 @@
-# Correct! "
-
+# Correct! (")
 message = "One of Python's strengths is its diverse community."
 print(message)
 
-# Incorrect! ' - Syntax Error
-
+# Incorrect! (') - Syntax Error
 message = 'One of Python's strengths is its diverse community.'
 print(message)
