e095bcd2eb268f1242582f86050c398f7d3d8c00
HaySim <hayley_simmons@outlook.com>
2020-06-18 12:04:54 +0100

    used pop() to remove guests from my list

--- chapter-3/guest_list.py
+++ chapter-3/guest_list.py
@@ -45,6 +45,28 @@ print(message)
 message = f"Dear {guests[-1].title()}, would you like to come for dinner on Saturday?"
 print(message)
 
+message = f"Hi {guests[0].title()}, change of plan, can you come for dinner on Sunday instead?"
+print(message)
+message = f"Hi {guests[2].title()}, change of plan, can you come for dinner on Sunday instead?"
+print(message)
+message = f"Hi {guests[-1].title()}, change of plan, can you come for dinner on Sunday instead?"
+print(message)
+
+guests = ['adam', 'mum', 'laura', 'dad', 'izunna', 'jackie']
+print(guests)
+
+message = f"Hi {guests[5].title()}, sorry to hear that you can't make it on Sunday."
+print(message)
+
+popped_guest = guests.pop(5)
+print(guests)
+
+message = f"Hi {guests[0].title()}, sorry to hear that you can't make it on Sunday."
+print(message)
+
+popped_guest = guests.pop(0)
+print(guests)
+
 
 
 
