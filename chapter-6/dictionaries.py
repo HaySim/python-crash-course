c7345ae0d82bdc7712eec3e405512c4cde66593c
HaySim <hayley_simmons@outlook.com>
2020-06-21 20:58:38 +0100

    using Python's dictionaries

---
+++ chapter-6/dictionaries.py
@@ -0,0 +1,23 @@
+alien_0 = {'colour': 'green', 'points': 5}
+print(alien_0['colour'])
+print(alien_0['points'])
+
+print("")
+
+# accessing values in a dictionary:
+# will print 'green'.
+alien_0 = {'colour': 'green'}
+print(alien_0['colour'])
+
+print("")
+
+# you can have unlimited number of key-value pairs in a dictionary.
+alien_0 = {'colour': 'green', 'points': 5}
+
+alien_0 = {'colour': 'green', 'points': 5}
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+print("")
+
+# adding new key-value pairs:
