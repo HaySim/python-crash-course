25ccfa08ecd538acb035ead70421b22dc7f9af7e
HaySim <hayley_simmons@outlook.com>
2020-06-21 20:47:54 +0100

    continue with if-elif-else statements

--- chapter-5/if_statements.py
+++ chapter-5/if_statements.py
@@ -63,3 +63,125 @@ elif age < 18:
 else:
     price = 40
 print(f"Your admission cost is £{price}.")
+
+# use as many elif blocks in your code as you like.
+# let's say anyone 65 or older pays half the regular admission, or £20:
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+print(f"Your admission cost is £{price}.")
+
+# Python does not require an else block at the end of an if-elif chain but sometimes an else block is useful.
+# omitting the else block:
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age >= 65:
+    price = 20
+print(f"Your admission cost is £{price}.")
+
+print("")
+
+# testing multiple conditions using a series of simple if statements with no elif or else blocks:
+# Python doesn't run any tests beyond the first test that passes in an if-elif-else chain.
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
+
+print("")
+
+alien_colour = ['green', 'red', 'yellow', 'blue']
+if 'green' in alien_colour:
+    print("Player earned 5 points.")
+print("\nYou win!")
+
+colour = "green"
+if "green" in colour:
+    points = 10
+if "red" in colour:
+    points = 5
+if "yellow" in colour:
+    points = 1
+if "blue" in colour:
+    points = 1
+print(f"You win {points} points!") 
+
+print("")
+
+colour = "green"
+if "green" in colour:
+    points = 10
+elif "red" in colour:
+    points = 5
+else:
+    points = 1
+print(f"You win {points} points!") 
+
+print("")
+
+age = 10
+if age < 2:
+    message = "Person is a baby."
+elif age >2 :
+    message = "Person is a toddler." 
+elif age > 4 : 
+    message = "Person is a child."
+elif age > 13:
+    message =  "Person is a teenager."
+else:
+    message = "Person is an adult."
+print(f"{message}")
+
+print("")
+
+# checking for special items:
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
+
+print("")
+
+# checking that a list is not empty:
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+print("")
+
+# using multiple lists:
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
